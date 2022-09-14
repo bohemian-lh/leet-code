8ef6c10c290cfe884ead3a269b9c1cb9f84f8f03
jxgm-clion <bohemian.lh@gmail.com>
2022-09-14 16:16:54 +0800

    create classic_class dir and moving three files(case-test\CowString.cpp WordFrequence.cpp Singleton.cpp) to that directory.

---
+++ classic_class/Blob.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2022/9/10.
+//
+
+#include "Blob.h"
---
+++ classic_class/Blob.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2022/9/10.
+//
+
+#ifndef LET_CODE_BLOB_H
+#define LET_CODE_BLOB_H
+
+template<typename T>
+class Blob{
+
+};
+
+
+#endif //LET_CODE_BLOB_H
